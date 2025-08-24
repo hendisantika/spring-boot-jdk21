7a103177161ce645171eb41a80d38d32f44e0eb4
hendisantika <hendisantika@yahoo.co.id>
2025-08-24 08:41:30 +0700

    feat: Add PostService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootjdk21/service/PostService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.springbootjdk21.service;
+
+import id.my.hendisantika.springbootjdk21.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jdk21
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 24/08/25
+ * Time: 08.41
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+}
