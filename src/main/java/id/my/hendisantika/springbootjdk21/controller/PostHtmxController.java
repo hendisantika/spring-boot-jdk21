e48c3c297329554dc29bee1ea8e8f487b5b53916
hendisantika <hendisantika@yahoo.co.id>
2025-08-25 04:38:27 +0700

    feat: Add getAllPosts in PostHtmxController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootjdk21/controller/PostHtmxController.java
+++ src/main/java/id/my/hendisantika/springbootjdk21/controller/PostHtmxController.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.springbootjdk21.controller;
 
+import id.my.hendisantika.springbootjdk21.entity.Post;
 import id.my.hendisantika.springbootjdk21.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +27,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PostHtmxController {
 
     private final PostService postService;
+
+    @GetMapping
+    public String getAllPosts(Model model) {
+        List<Post> posts = postService.getAllPosts();
+
+        model.addAttribute("title", "Blog Page");
+        model.addAttribute("content", "all-posts");
+        model.addAttribute("posts", posts);
+        return "index";
+    }
 }
