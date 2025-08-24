df717729bbcfdc62d38322098a146773b355b139
hendisantika <hendisantika@yahoo.co.id>
2025-08-24 08:35:30 +0700

    feat: Add SignInDTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootjdk21/dto/SignInDTO.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.springbootjdk21.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jdk21
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 24/08/25
+ * Time: 08.35
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class SignInDTO {
+    private String email;
+    private String password;
+}
