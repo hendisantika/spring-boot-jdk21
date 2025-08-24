70b71550b89afce4716954ed6fb192d44bf101e4
hendisantika <hendisantika@yahoo.co.id>
2025-08-24 08:39:58 +0700

    feat: Add FileStorageService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootjdk21/service/FileStorageService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.springbootjdk21.service;
+
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jdk21
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 24/08/25
+ * Time: 08.39
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class FileStorageService {
+    private final Path rootLocation = Paths.get("uploads");
+
+}
