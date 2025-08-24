d5391bb44a5ca5f6830bc0b6d0fb5efb6f418618
hendisantika <hendisantika@yahoo.co.id>
2025-08-25 04:34:43 +0700

    feat: Add uploadImage in FileController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootjdk21/controller/FileController.java
+++ src/main/java/id/my/hendisantika/springbootjdk21/controller/FileController.java
@@ -2,8 +2,14 @@ package id.my.hendisantika.springbootjdk21.controller;
 
 import id.my.hendisantika.springbootjdk21.service.FileStorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class FileController {
 
     private FileStorageService fileStorageService;
+
+    @PostMapping("/upload/image")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            String filename = fileStorageService.saveFile(file, "image");
+            return ResponseEntity.ok("Image uploaded successfully: " + filename);
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().body("Failed to upload image: " + e.getMessage());
+        }
+    }
 }
