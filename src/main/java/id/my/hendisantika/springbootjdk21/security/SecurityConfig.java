91be557aa956ea45d0f48b61600a48371f48c737
hendisantika <hendisantika@yahoo.co.id>
2025-08-24 08:37:04 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootjdk21/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.springbootjdk21.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jdk21
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 24/08/25
+ * Time: 08.36
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
