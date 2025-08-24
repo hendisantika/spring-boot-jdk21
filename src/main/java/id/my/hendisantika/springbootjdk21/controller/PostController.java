65c7d5f847df109c19884499c6a78736df876d66
hendisantika <hendisantika@yahoo.co.id>
2025-08-25 04:37:00 +0700

    feat: Add createPost in PostController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootjdk21/controller/PostController.java
+++ src/main/java/id/my/hendisantika/springbootjdk21/controller/PostController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,5 +59,10 @@ public class PostController {
     @GetMapping("/{id}")
     public Post getPostById(@PathVariable Long id) {
         return postService.getPostById(id);
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postService.createPost(post);
     }
 }
