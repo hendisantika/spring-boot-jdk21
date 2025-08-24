b47c6a646049ed70b1ff481973bdbf210a7a6c0d
hendisantika <hendisantika@yahoo.co.id>
2025-08-25 04:36:29 +0700

    feat: Add getAllPosts in PostController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootjdk21/controller/PostController.java
+++ src/main/java/id/my/hendisantika/springbootjdk21/controller/PostController.java
@@ -1,10 +1,20 @@
 package id.my.hendisantika.springbootjdk21.controller;
 
+import id.my.hendisantika.springbootjdk21.entity.Post;
+import id.my.hendisantika.springbootjdk21.entity.User;
+import id.my.hendisantika.springbootjdk21.security.RoleEnum;
 import id.my.hendisantika.springbootjdk21.service.AuthService;
 import id.my.hendisantika.springbootjdk21.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +34,23 @@ public class PostController {
 
     private final AuthService authService;
     private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllPosts(HttpServletRequest request) {
+        Optional<User> userOptional = authService.getUserFromSession(request);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            List<Post> posts = postService.getAllPosts();
+
+            if (user.getRoleId() == RoleEnum.ADMIN.getRoleId()) {
+                posts.forEach(post -> post.setText("ADMIN - " + post.getText()));
+            }
+
+            return new ResponseEntity<>(posts, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+    }
+
 }
