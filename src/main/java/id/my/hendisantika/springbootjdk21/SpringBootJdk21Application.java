03de71605ec3b956e783f8144169952032f79044
hendisantika <hendisantika@yahoo.co.id>
2025-08-24 08:25:04 +0700

    feat: Init Project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootjdk21/SpringBootJdk21Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootjdk21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdk21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdk21Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootjdk21/SpringBootJdk21ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootjdk21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdk21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
